08a7c3547fa02fec73e638c2bbb14a3365a768cf
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-02-26 10:12:18 -0800

    base done

--- counter/counter/ContentView.swift
+++ counter/counter/ContentView.swift
@@ -7,15 +7,38 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var count : Int = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Click buttons below to change the counter")
+                .padding()
+            Text("Count: \(count)")
         }
+        HStack {
+            Button("Increment", action: addOne)
+                .padding()
+                . background(Color.blue)
+                . foregroundColor(.white)
+                . cornerRadius(10)
+            Button("Decrement", action: minusOne)
+                .padding()
+                . background(Color.blue)
+                . foregroundColor(.white)
+                . cornerRadius(10)
+
+        }
+        
         .padding()
+    }
+    func addOne(){
+        count += 1
+    }
+    func minusOne(){
+        if count > 0{
+            count -= 1
+        }
     }
 }
 
